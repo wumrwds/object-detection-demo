699fe59d863bc4b2fe62181d67a38c8aace5762c
Minreng Wu <wumrwds@gmail.com>
2020-05-29 16:54:47 -0500

    chore: initialize backend project

---
+++ backend/src/main/java/com/github/wumrwds/ods/OdsDemoApplication.java
@@ -0,0 +1,13 @@
+package com.github.wumrwds.ods;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OdsDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OdsDemoApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/github/wumrwds/ods/OdsDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.wumrwds.ods;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OdsDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
