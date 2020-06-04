73ec50e1ec031529540b887bf5da1eb8d995d33d
Minreng Wu <wumrwds@gmail.com>
2020-06-04 11:23:38 -0500

    chore: initialize statistics page

---
+++ frontend/src/components/common/Stats.vue
@@ -0,0 +1,132 @@
+<template>
+<div class="list">
+        <el-scrollbar class="page-component__scroll">
+            <el-main>
+                <el-breadcrumb separator-class="el-icon-arrow-right">
+                    <el-breadcrumb-item :to="{ path: '/' }">Homepage</el-breadcrumb-item>
+                    <el-breadcrumb-item>Stats</el-breadcrumb-item>
+                </el-breadcrumb>
+                <el-table
+                    :data="tableData"
+                    stripe
+                    style="width: 100%">
+                    <el-table-column
+                    prop="date"
+                    label="Date"
+                    width="180">
+                    </el-table-column>
+                    <el-table-column
+                    prop="name"
+                    label="Name"
+                    width="180">
+                    </el-table-column>
+                    <el-table-column
+                    prop="address"
+                    label="Address">
+                    </el-table-column>
+                </el-table>
+            </el-main>
+        </el-scrollbar>
+</div>
+</template>
+
+<script>
+export default {
+    data () {
+        return {
+            tableData: [{
+                date: '2016-05-03',
+                name: 'Tom',
+                address: 'No. 189, Grove St, Los Angeles'
+            }, {
+                date: '2016-05-02',
+                name: 'Tom',
+                address: 'No. 189, Grove St, Los Angeles'
+            }, {
+                date: '2016-05-04',
+                name: 'Tom',
+                address: 'No. 189, Grove St, Los Angeles'
+            }, {
+                date: '2016-05-01',
+                name: 'Tom',
+                address: 'No. 189, Grove St, Los Angeles'
+            }]
+        }
+    }
+}
+</script>
+<style lang="scss" scoped>
+
+.list{
+    height: 100%;
+    color: #48576a;
+    .header-box {
+		background-image: linear-gradient(-180deg, rgba(37,30,71,.9) 0%, rgba(36,83,155,.9) 100%);
+		@media (max-width: 1249px) {
+            width: 1080px !important;
+            margin: 0 auto;
+        }
+    }
+    .page-component__scroll{
+        height: 100%;
+        .el-breadcrumb{
+            padding-bottom: 20px;
+            border-bottom: 1px solid #ebeef5;
+        }
+    }
+    .el-main{
+        .el-breadcrumb{
+            padding-bottom: 20px;
+            border-bottom: 1px solid #ebeef5;
+        }
+        .title-section{
+            background: #f0f2f5;
+            padding: 10px;
+            margin: 20px 0;
+            position: relative;
+            color: #606266;
+            .item-group{
+                display: inline-block;
+                margin-right: 16px;
+                label{
+                    padding-right: 6px;
+                }
+            }
+            .el-radio-button__inner{
+                padding: 10px
+            }
+            .el-input{
+                width: 230px;
+            }
+        }
+        .query-button{
+            position: absolute;
+            right:90px;
+        }
+        .add-button{
+            position: absolute;
+            right: 10px;
+        }
+        .el-button{
+            a{
+                text-decoration: none;
+                color: #fff;
+            }
+            &:hover{
+
+            }
+        }
+        .el-table{
+            color: #606266;
+            thead{
+                 color: #606266;
+            }
+        }
+        .el-pagination{
+            margin-top:30px;
+            text-align: right;
+        }
+    }
+}
+
+</style>
